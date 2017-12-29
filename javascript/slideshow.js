90c15a1e397ca42cab18ce8ced90a39f9a4bee55
kristian <p1417693x@my365.dmu.ac.uk>
2017-12-29 17:13:56 +0000

    maps updated

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,19 +1,27 @@
 
-var image1=new Image()
-image1.src="orient.jpg"
-var image2=new Image()
-image2.src="star_wars.jpg"
-var image3=new Image()
-image3.src="justiceleague.jpg"
 
+    "use strict";
+    function initMap() {
+      let london = {lat: 51.521296, lng: -0.131818};
+      let bigben = {lat: 51.500889, lng: -0.124625};
+      let leics = {lat: 51.510805, lng: -0.129599};
+      let mapDemo = document.getElementById("map");
 
-var step=1
-function slideit(){
-document.images.slide.src=eval("image"+step+".src")
-if(step<3)
-step++
-else
-step=1
-setTimeout("slideit()",2500)
-}
-slideit()
+      // this sets the default location for when the map is first loaded
+      let map = new google.maps.Map(mapDemo, {
+        zoom: 12,
+        center: london
+      });
+
+      // these set different markers you want to show on your map
+      let markerBIGBEN = new google.maps.Marker({
+        position: bigben,
+        map: map,
+        title: 'Icon'
+      });
+      let markerleics = new google.maps.Marker({
+        position: leics,
+        map: map,
+        title: 'Movie Central'
+      });
+    }
